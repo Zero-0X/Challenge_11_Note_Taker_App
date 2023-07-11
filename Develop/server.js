6b9e507c9ed9c1debd25ccb52fb1ff92ad9c59c5
Zero_X <rvkov536@gmail.com>
2023-07-11 10:00:46 -0400

    worked with instructor to determine source of PORT issue, killed port, then changed to 3333 for development purposes, also updated get to correctly source notes from db json file

--- Develop/server.js
+++ Develop/server.js
@@ -3,33 +3,29 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 52330;
+const PORT = process.env.PORT || 3333;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/', (clientReq, serverRes) => {
-  serverRes.sendFile(path.join(__dirname, '/public/index.html'));
-});
-
 app.get('/notes', (clientReq, serverRes) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'));
+  serverRes.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 app.get('/api/notes', (clientReq, serverRes) => {
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+  fs.readFile('./db/db.json', (err, data) => {
     if (err) {
       console.error(err);
       return serverRes.status(500).json({ error: 'Error reading notes.' });
     }
     const notes = JSON.parse(data);
-    res.json(notes);
+    serverRes.json(notes);
   });
 });
 
 app.post('/api/notes', (clientReq, serverRes) => {
-  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+  fs.readFile('./db/db.json', (err, data) => {
     if (err) {
       console.error(err);
       return serverRes.status(500).json({ error: 'Error reading notes.' });
